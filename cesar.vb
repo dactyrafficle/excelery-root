ea7cab1cb39bf24bb7e9b40fbebca76c37a01e5b
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2019-11-03 23:35:09 -0500

    Add files via upload

--- cesar.vb
+++ cesar.vb
@@ -1,6 +1,4 @@
-'the start of a simple cesar shift; maybe focus on a range of first 0-127 chars from unicode
-'no error for missing shift - needed
-'how shall i do the reverse? a separate function or a parameter? 
+Attribute VB_Name = "Module1"
 
 Function cesar(str As String, shift As Integer) As String
 
@@ -9,16 +7,34 @@ Function cesar(str As String, shift As Integer) As String
     Dim bytes() As Byte
     bytes = StrConv(str, vbFromUnicode) 'an array of bytes
     
+    'the range of acceptable chars will be unicode 32 to 126, for a range of 95 chars
+    '26 lower, 26 upper, 10 digits, 33 spec chars = 95 chars
+    Dim minCharNo, maxCharNo, charSpace As Integer
+    minCharNo = 32
+    maxCharNo = 126
+    charSpace = maxCharNo - minCharNo + 1
+    
     Dim output As String
     output = ""
     
     Dim i As Integer
     For i = 0 To UBound(bytes)
     
-        output = output & Chr(bytes(i) + shift)
+        Dim newCharNumber As Integer
+        
+        'vba has a buggy mod fn that return weird for negative values of a so i write my own, is easy enuf
+    
+        'c = mod(a, b) or c = (a)mod(b)
+        Dim a, b, c As Integer
+        a = Int((bytes(i) - minCharNo + shift)) 'convert range from 32-127 to 0-94, then shift
+        b = charSpace
+        c = a - Int(a / b) * b
+        newCharNumber = c + minCharNo 'reconvert range to 32-127
+        output = output & Chr(newCharNumber)
     
     Next i
     
     cesar = output
 
 End Function
+
