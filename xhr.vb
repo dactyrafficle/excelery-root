ae3af8f17062e722f8a4d4189fb05cd0679f2bb5
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2019-11-14 23:31:49 -0500

    Update xhr.vb

--- xhr.vb
+++ xhr.vb
@@ -7,10 +7,12 @@ Sub abc()
     Dim xhr As New MSXML2.XMLHTTP60
     
     Dim url As String
-    url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=0c5b40099f42275292567c7af6d887b9"
+    'url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=0c5b40099f42275292567c7af6d887b9"
+    url = "http://shakespeare.mit.edu/romeo_juliet/romeo_juliet.1.0.html"
     xhr.Open "GET", url, False
     xhr.send
     
-    MsgBox xhr.responseText
+    'in this case, the html document comes back as a big string - so i have to parse it
+    Debug.Print xhr.responseText
 
 End Sub
