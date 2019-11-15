0707bb2b3109ac05e8808fd96221f602b0a316ee
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2019-11-15 00:16:51 -0500

    Create traverseRange.vb

---
+++ traverseRange.vb
@@ -0,0 +1,11 @@
+'range.rows is a collection
+'so range.rows.count returns a long, which represents the number of row objects in the rows collection
+
+'you could do range.cells(x,y) or range.rows.cells(x,y) or range.columns.cells(x,y)
+'tho u can also do range(i) but then you are counting cell by cell in the range
+
+sub abc()
+
+  
+
+end sub
