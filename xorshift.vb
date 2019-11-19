837a3ffc90a63965c54f9cfa82d3e21300482fab
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2019-11-18 20:26:47 -0500

    Update xorshift.vb

--- xorshift.vb
+++ xorshift.vb
@@ -3,12 +3,13 @@ Function xorShift(str1 As String, str2) As String
     'xorshift the 1st string by the values of the 2nd
 
     'convert str1 and str2 into byte arrays
-    Dim msg() As Byte, pass() As Byte
+    Dim msg() As Byte, pass() As Byte, out() As Byte
     msg = StrConv(str1, vbFromUnicode)
     pass = StrConv(str2, vbFromUnicode)
+    out = StrConv(str1, vbFromUnicode)
     
-    Dim output As String
-    output = ""
+    'Dim output As String
+    'output = ""
     
     Dim i As Integer
     For i = LBound(msg) To UBound(msg)
@@ -18,17 +19,16 @@ Function xorShift(str1 As String, str2) As String
 
         b = UBound(pass) + 1
         x2 = pass(i Mod b)
-    
-    'so this does work, but i think 97 xor 97 is a bad result i think - how to fix
-        Debug.Print x1 & " xor " & x2
         
-        ' ie. 10 Xor 8 ->1010 Xor 1000 -> 0010 = 2
-        y = x1 Xor x2
+        out(i) = x1 Xor x2
         
-        output = output & Chr(y)
+        'Debug.Print x1 & " xor " & x2; " -> " & out(i)
     
     Next i
     
-    xorShift = output
+    Debug.Print UBound(out) - LBound(out) + 1
+    
+    ' xorshift("abcde", "ab") produces a value with length 0. i want a blank with length 5
+    xorShift = StrConv(out, vbUnicode)
 
 End Function
