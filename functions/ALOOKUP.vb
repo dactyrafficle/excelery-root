5014252112cf6f6c06cec1a5499593f366a8e778
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2019-11-27 22:22:19 -0500

    Update ALOOKUP.vb

--- functions/ALOOKUP.vb
+++ functions/ALOOKUP.vb
@@ -3,7 +3,13 @@ Option Explicit
 'accepts ranges and arrays
 'isExact is only there to keep the same arg sequence as vlookup
 Function ALOOKUP(master As Variant, a As Variant, x As Long, isExact As Boolean, returnAsCollection As Boolean) As Variant
-
+    
+    'master references a range, when does it become a value?
+    If IsEmpty(master) Then
+        ALOOKUP = 999999
+        Exit Function
+    End If
+        
     'if a is a range, shrink before converting to an array
     Dim arr As Variant
     If TypeName(a) = "Range" Then
@@ -15,7 +21,6 @@ Function ALOOKUP(master As Variant, a As Variant, x As Long, isExact As Boolean,
         arr = a
     End If
 
-    
     'loop over the first column, and look for master
     Dim i As Long
     Dim temp As New Collection 'to store successful results
@@ -25,6 +30,10 @@ Function ALOOKUP(master As Variant, a As Variant, x As Long, isExact As Boolean,
         End If
     Next i
     
+    If temp.Count = 0 Then
+        temp.Add 999999
+    End If
+        
     If returnAsCollection Then
         ALOOKUP = temp
     Else
