686fa22491e6de9ed75c973f776363e82e81156d
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2019-12-19 15:58:14 -0500

    Add files via upload

--- pandatax/changeImages.js
+++ pandatax/changeImages.js
@@ -1,18 +1,21 @@
 console.log(Math.random());
 
+let imgURL1 = "";
+let imgURL2 = "https://i.imgur.com/CJcN9iQ.png";
+let imgURL3 = "";
 
 (function(){
 	let imgs = document.getElementsByTagName('source');
 	for (let i = 0; i < imgs.length; i++) {
-		imgs[i].srcset = "";
+		imgs[i].srcset = imgURL2;
 	}	
 })();
 
 (function(){
 	let imgs = document.getElementsByTagName('img');
 	for (let i = 0; i < imgs.length; i++) {
-		imgs[i].src = ""
-		imgs[i].srcset = ""
+		imgs[i].src = imgURL2;
+		imgs[i].srcset = imgURL2;
 	}
 })();
 
