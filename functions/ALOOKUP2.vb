9eb15e8d17b33adb3da6143e18fe121db87fe44b
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2020-02-10 22:27:07 -0500

    Add files via upload

--- functions/ALOOKUP2.vb
+++ functions/ALOOKUP2.vb
@@ -2,42 +2,34 @@ Option Explicit
 
 'accepts ranges and arrays
 'isExact is only there to keep the same arg sequence as vlookup
-Function ALOOKUP2(master As Variant, a As Variant, x As Long, isExact As Boolean, returnAsCollection As Boolean) As Variant
-    
-    'master references a range, when does it become a value?
-    If IsEmpty(master) Then
-        ALOOKUP = 999999
-        Exit Function
-    End If
+'return as array is preferable over collection bc sum/count work on an array
+Function ALOOKUP2(master As Variant, arr_ As Variant, x As Long, isExact As Boolean, returnAsArray As Boolean) As Variant
         
-    'if a is a range, shrink before converting to an array
-    Dim arr As Variant
-    If TypeName(a) = "Range" Then
-        Dim r As Range
-        Set r = Intersect(a.Parent.UsedRange, a)
-        arr = r.Value
-        Debug.Print UBound(arr, 1)
-    Else
-        arr = a
-    End If
+  'IF arr_ IS RANGE, SHRINK AND CONVERT TO ARR
+  Dim arr As Variant
+  If TypeName(arr_) = "Range" Then
+    Dim r As Range
+    Set r = Intersect(arr_.Parent.UsedRange, arr_)
+    arr = r.Value
+  Else
+    arr = arr_
+  End If
 
-    'loop over the first column, and look for master
-    Dim i As Long
-    Dim temp As New Collection 'to store successful results
-    For i = LBound(arr, 1) To UBound(arr, 1)
-        If arr(i, 1) = master Then
-           temp.Add arr(i, x)
-        End If
-    Next i
-    
-    If temp.Count = 0 Then
-        temp.Add 999999
+  'loop over the first column, and look for master
+  Dim i As Long, count As Long, temp() As Variant 'to store results
+  count = 0
+  For i = LBound(arr, 1) To UBound(arr, 1)
+    If arr(i, 1) = master Then
+      count = count + 1
+      ReDim Preserve temp(count)
+      temp(count) = arr(i, x)
     End If
-        
-    If returnAsCollection Then
-        ALOOKUP = temp
-    Else
-        ALOOKUP = temp(1)
-    End If
+  Next i
     
+  If returnAsArray Then
+    ALOOKUP2 = temp
+  Else
+    ALOOKUP2 = temp(1)
+  End If
+  
 End Function
