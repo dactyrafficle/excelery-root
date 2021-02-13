2c0f67625f5083a0fb20cc9fe2ebf815d93a80a2
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2021-02-13 17:03:00 -0500

    Update STR_SPLIT.vb

--- functions/STR_SPLIT.vb
+++ functions/STR_SPLIT.vb
@@ -21,9 +21,9 @@ Public Function STR_SPLIT(str As String, ParamArray delimiters() As Variant) As 
  Dim y As Long, x As Long
  For y = LBound(bytes) To UBound(bytes)
  
- For x = LBound(delimiters) To UBound(delimiters)
-  If (Chr(bytes(y)) = delimiters(x)) Then bytes(y) = 0
- Next x
+  For x = LBound(delimiters) To UBound(delimiters)
+   If (Chr(bytes(y)) = delimiters(x)) Then bytes(y) = 0
+  Next x
   
   If (y = LBound(bytes)) Then 'IS FIRST CHAR
   
